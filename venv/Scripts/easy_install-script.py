0faae347442130016b1efda1e679828afccb14cb
Bryan Respass <bryres@bergen.org>
2019-01-09 12:12:11 -0500

    test

---
+++ venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\bryres\Google Drive\Classes\2017-18\Intro_to_cs\Python\Unit7-MidyearProject\untitled\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install'
+__requires__ = 'setuptools==39.1.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==39.1.0', 'console_scripts', 'easy_install')()
+    )
---
+++ venv/Scripts/pip3-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\bryres\Google Drive\Classes\2017-18\Intro_to_cs\Python\Unit7-MidyearProject\untitled\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==10.0.1','console_scripts','pip3'
+__requires__ = 'pip==10.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==10.0.1', 'console_scripts', 'pip3')()
+    )
